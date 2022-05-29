3656a0edf6c1cc25bf62d49bfc303e747c2291af
Maxgeforce <maximiliano.fernandez11@gmail.com>
2022-05-29 15:29:50 -0300

    First File

--- src/Files/FileManagement.java
+++ src/Files/FileManagement.java
@@ -1,4 +1,23 @@
 package Files;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import com.google.gson.Gson;
+
 public class FileManagement {
+
+    public static void escribiendoArchivo() throws IOException {
+
+        try {
+          BufferedWriter buffer = new BufferedWriter(new FileWriter(new File("prueba.txt")));
+          buffer.write("Vayanse todos a la re calcada re concha de su madre");
+
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
 }
+
