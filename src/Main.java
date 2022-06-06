520857fc91945b9aa5780b89a9cb94a9127e0872
Leandro Lorenzo <leandromartinlorenzo@gmail.com>
2022-06-06 20:06:58 -0300

    modularizó menu
    se crearon opciones para llamar dentro de los case. Dentro de estas opciones, van a escribirse los llamados a otras clases

--- src/Main.java
+++ src/Main.java
@@ -22,34 +22,28 @@ public class Main {
 
         Scanner scan = new Scanner(System.in);
         int respuesta;
+
         do {
-            System.out.println("<<< Bienvenidos a AeroTaxi >>>");
-            System.out.println("1- AeroTaxi Bronze");
-            System.out.println("2- AeroTaxi Plata");
-            System.out.println("3- AeroTaxi Gold");
-            System.out.println("0- ESC");
+            opcionIntro();
             respuesta = scan.nextInt();
             switch (respuesta){
                 case 1:
                     System.out.println("Bronze en proceso..");
                     do {
-                        System.out.println("Elija ubicación:");
-                        System.out.println("1- Ventana");
-                        System.out.println("2- Pasillo");
-                        System.out.println("0- ESC");
+                        opcion0();
                         respuesta = scan.nextInt();
                         switch (respuesta){
                             case 1:
-                                System.out.println("con ventana");
+                                opcion1();
                                 break;
                             case 2:
-                                System.out.println("en pasillo");
+                                opcion2();
                                 break;
                             case 0:
-                                System.out.println("ESC");
+                                opcion3();
                                 break;
                             default:
-                                System.out.println("Solo puede elegir las opciones 1 o 2");
+                                opcionDefault();
                                 break;
                         }
                     }while (respuesta != 0);
@@ -57,23 +51,20 @@ public class Main {
                 case 2:
                     System.out.println("Plata en proceso..");
                     do {
-                        System.out.println("Elija confort:");
-                        System.out.println("1- Con frazada");
-                        System.out.println("2- Con almohada");
-                        System.out.println("0- ESC");
+                        opcion4();
                         respuesta = scan.nextInt();
                         switch (respuesta){
                             case 1:
-                                System.out.println("con frazada entonces");
+                                opcion5();
                                 break;
                             case 2:
-                                System.out.println("con almohada entonces");
+                                opcion6();
                                 break;
                             case 0:
-                                System.out.println("ESC");
+                                opcion3();
                                 break;
                             default:
-                                System.out.println("Solo puede elegir las opciones 1 o 2");
+                                opcionDefault();
                                 break;
                         }
                     }while (respuesta != 0);
@@ -81,23 +72,20 @@ public class Main {
                 case 3:
                     System.out.println("Gold en proceso..");
                     do {
-                        System.out.println("Elija Comunicación:");
-                        System.out.println("1- Con WiFi");
-                        System.out.println("2- Sin WiFi");
-                        System.out.println("0- ESC");
+                        opcion7();
                         respuesta = scan.nextInt();
                         switch (respuesta){
                             case 1:
-                                System.out.println("con WiFi entonces");
+                                opcion8();
                                 break;
                             case 2:
-                                System.out.println("sin WiFi entonces");
+                                opcion9();
                                 break;
                             case 0:
-                                System.out.println("ESC");
+                                opcion3();
                                 break;
                         default:
-                            System.out.println("Solo puede elegir las opciones 1 o 2");
+                            opcionDefault();
                             break;
                         }
                     }while (respuesta != 0);
@@ -110,5 +98,64 @@ public class Main {
         System.out.println("despegue !!");
     }
 
+    private static void case1(){
+
+    }
+    private static void case2(){
+
+    }
+    private static void case3(){
+
+    }
+    private static void opcionIntro() {
+        System.out.println("<<< Bienvenidos a AeroTaxi >>>");
+        System.out.println("1- AeroTaxi Bronze");
+        System.out.println("2- AeroTaxi Plata");
+        System.out.println("3- AeroTaxi Gold");
+        System.out.println("0- ESC");
+    }
+    private static void opcion0() {
+        System.out.println("Elija ubicación:");
+        System.out.println("1- Ventana");
+        System.out.println("2- Pasillo");
+        System.out.println("0- ESC");
+    }
+    private static void opcion1() {
+        System.out.println("con ventana");
+    }
+    private static void opcion2() {
+        System.out.println("en pasillo");
+    }
+    private static void opcion3() {
+        System.out.println("ESC");
+    }
+    private static void opcionDefault() {
+        System.out.println("Solo puede elegir las opciones 1 o 2");
+    }
+    private static void opcion4() {
+        System.out.println("Elija confort:");
+        System.out.println("1- Con frazada");
+        System.out.println("2- Con almohada");
+        System.out.println("0- ESC");
+    }
+    private static void opcion5() {
+        System.out.println("con frazada entonces");
+    }
+    private static void opcion6() {
+        System.out.println("con almohada entonces");
+    }
+    private static void opcion7() {
+        System.out.println("Elija Comunicación:");
+        System.out.println("1- Con WiFi");
+        System.out.println("2- Sin WiFi");
+        System.out.println("0- ESC");
+    }
+    private static void opcion8() {
+        System.out.println("con WiFi entonces");
+    }
+    private static void opcion9() {
+        System.out.println("sin WiFi entonces");
+    }
 }
+
 
